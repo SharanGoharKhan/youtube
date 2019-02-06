a2be059e060a68ebe828b2cc189cc7c793940e18
adnan909 <adnan@ninjascode.com>
2019-02-06 19:45:25 +0500

    remove https

--- index.js
+++ index.js
@@ -22,20 +22,20 @@ const port = process.env.PORT || 3000
 // the "X-Forwarded-Proto" header field to be trusted so its
 // value can be used to determine the protocol. See 
 // http://expressjs.com/api#app-settings for more details.
-app.enable('trust proxy');
+// app.enable('trust proxy');
 
-// Add a handler to inspect the req.secure flag (see 
-// http://expressjs.com/api#req.secure). This allows us 
-// to know whether the request was via http or https.
-app.use(function (req, res, next) {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-    } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-});
+// // Add a handler to inspect the req.secure flag (see 
+// // http://expressjs.com/api#req.secure). This allows us 
+// // to know whether the request was via http or https.
+// app.use(function (req, res, next) {
+//     if (req.secure) {
+//         // request was via https, so do no special handling
+//         next();
+//     } else {
+//         // request was via http, so redirect to https
+//         res.redirect('https://' + req.headers.host + req.url);
+//     }
+// });
 
 
 mongoose.Promise = global.Promise
